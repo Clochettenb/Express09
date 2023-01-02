202a3504af34265d5fd85f108bb9c78b9423c498
Chloé Bidau <chloebidau@hotmail.fr>
2023-01-02 11:35:37 +0100

    finish the express 09.1

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,12 @@
 const express = require("express");
 
+const fs = require("fs");
+
 const multer = require("multer");
+
+const { v4: uuidv4 } = require("uuid");
+
+const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 
@@ -11,5 +17,24 @@ router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+// route POST pour recevoir un fichier
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  // On récupère le nom du fichier
+  const { originalname } = req.file;
+
+  // On récupère le nom du fichier
+  const { filename } = req.file;
+
+  // On utilise la fonction rename de fs pour renommer le fichier
+  fs.rename(
+    `uploads/${filename}`,
+    `uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 module.exports = router;
